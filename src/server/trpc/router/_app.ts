4963d34e555383fe6007204ca3980d29713d1897
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2022-11-19 04:01:29 +0100

    (Chore) Removed example files

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,10 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { usersRouter } from "./users";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
+	user: usersRouter,
 });
 
 // export type definition of API
