6d2b22c4f2c5f499607ac501248621d30a4ec4ff
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2022-11-20 18:52:00 +0100

    (Boards) Created styles for cards that will redirect to boards

--- src/pages/boards/index.tsx
+++ src/pages/boards/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 import React from "react";
 import Body from "../../components/body";
 
@@ -12,8 +13,9 @@ const Boards: NextPage = () => {
 		</Head>
 		<Body>
 			{/* Here goes navbar when done */}
-			{/* Banner + Public boards */}
+			{/* Banner */}
 			<UserBoards />
+			{/* Boards Were user is a member but not owner */}
 		</Body>
 	</>);
 };
@@ -25,43 +27,45 @@ const UserBoards: React.FC = () => {
 
 	const { data } = useSession();
 	if (!data) {
-		return <></>;
+		return (
+			<div className="container flex flex-col justify-center items-center text-center">
+				<p>Login to see your boards here!</p>
+			</div>
+		);
 	}
 	if (!data.user) {
 		return <></>;
 	}
 
+	const boards = [
+		{ "title": "Sample A", "link": "#" },
+		{ "title": "Sample B", "link": "#" },
+		{ "title": "Sample C", "link": "#" },
+		{ "title": "Sample D", "link": "#" },
+	];
+
 	return (
 		<div className="container flex flex-col p-2 justify-between items-center text-center">
 			<h2 className="text-4xl">Your boards!</h2>
 			<div className="w-full h-2 bg-slate-600 my-3 rounded-md"></div>
-			<div className="container w-full grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-				<div>
-					Sample Board Item AA
-				</div>
-				<div>
-					Sample Board Item AB
-				</div>
-				<div>
-					Sample Board Item AC
-				</div>
-				<div>
-					Sample Board Item AD
-				</div>
-				<div>
-					Sample Board Item BA
-				</div>
-				<div>
-					Sample Board Item BB
-				</div>
-				<div>
-					Sample Board Item BC
-				</div>
-				<div>
-					Sample Board Item BD
-				</div>
+			<div className="container w-full grid md:grid-cols-2 grid-cols-1 gap-5">
+				{
+					boards.map((b, i) => <BoardCard key={i} content={b.title} href={b.link} />)
+				}
 			</div>
 		</div>
+	);
+};
+
+const BoardCard: React.FC<{ content: string, href: string; }> = ({ content, href }) => {
+	return (
+		<Link
+			href={href}
+		>
+			<div className="flex flex-row relative text-center items-center py-4 h-32 bg-opacity-50 hover:bg-opacity-100 bg-slate-300 text-white hover:text-violet-900 rounded-lg shadow-inner hover:shadow-2xl transition-all duration-200 hover:scale-105 hover:outline outline-2 outline-purple-600">
+				<h2 className="text-2xl text-center w-full select-none">{content}</h2>
+			</div>
+		</Link>
 	);
 }
 
