c0b9b20a7822475b77c8d3f5d41c83a8eac9e527
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2022-11-20 15:13:57 +0100

    (NextAuth) DB adapter works now?

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider, { type DiscordProfile } from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import type { User as DBUser } from "prisma/prisma-client";
 
 import Credentials from "next-auth/providers/credentials";
 import { env } from "../../../env/server.mjs";
@@ -13,24 +14,43 @@ const maxAge = 43_200;
 
 export const authOptions: NextAuthOptions = {
 	session: {
-		// I'll be honest, no idea how to set it up with NextAuth's prisma adapter thingy 😭
-		strategy: "jwt",
 		maxAge,
 		updateAge: maxAge - 60 * 30, // refresh 30 minutes before expiring
 	},
-	// Include user.id on session
 	callbacks: {
 		async session({ session, user, token }) {
-			console.log(user);
 			if (session.user) {
-				if (token) {
+				if (token && !user) {
 					session.user.name = token.name;
 					session.user.image = token.picture;
+					session.user.email = token.email;
+					if (!token.name && !token.email) {
+						return session;
+					}
+					user = await prisma.user.findFirst({
+						select: {
+							id: true,
+							username: true,
+							email: true,
+							image: true,
+						},
+						where: {
+							OR: {
+								username: token.name as string,
+								email: token.email as string,
+							}
+						}
+					}) as User;
+					if (user) {
+						user.name = token.name
+					} else {
+						console.error("No user found!");
+					}
 				}
 				if (user) {
 					session.user.id = user.id;
-					session.user.name = user.name ?? session.user.name;
-					session.user.image = user.image ?? session.user.image;
+					session.user.name = user.name ?? token.name ?? session.user.name ?? "Anon";
+					session.user.image = user.image ?? session.user?.image ?? "";
 				}
 			}
 			return session;
@@ -47,12 +67,12 @@ export const authOptions: NextAuthOptions = {
 				username: { type: "string", placeholder: "xXNoobSlayer3000Xx" },
 				password: { type: "password", placeholder: "Blues Eye White Dragon I choose you!" }
 			},
-			async authorize(credentials, req) {
-				console.log(req);
+			async authorize(credentials) {
 				if (!credentials) {
 					return null;
 				}
-				const user = await prisma.user.findUnique({
+				// My VSCode sometimes fakely says this lacks a type IDK man
+				const user:DBUser|null = await prisma.user.findUnique({
 					where: {
 						username: credentials.username,
 					}
@@ -85,6 +105,7 @@ export const authOptions: NextAuthOptions = {
 			authorization: `https://discordapp.com/oauth2/authorize?&client_id=${env.DISCORD_CLIENT_ID}&scope=identify+email+connections`,
 			clientId: env.DISCORD_CLIENT_ID,
 			clientSecret: env.DISCORD_CLIENT_SECRET,
+			// allowDangerousEmailAccountLinking: true, // Technically a security issue but not sure how to setup connections properly too
 			async profile(profile:DiscordProfile):Promise<User> {
 				if (profile.avatar === null) {
 					const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
@@ -93,6 +114,7 @@ export const authOptions: NextAuthOptions = {
 					const format = profile.avatar.startsWith("a_") ? "gif" : "png";
 					profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
 				}
+				console.log("Calling profile method for DiscordProvider! Are we here before hitting page properly?")
 				const user = await prisma.user.findFirst({
 					where: {
 						email: profile.email,
@@ -106,6 +128,7 @@ export const authOptions: NextAuthOptions = {
 							image: profile.image_url,
 						}
 					});
+					this.allowDangerousEmailAccountLinking = true;
 					return Object.assign({}, newUser, { name: newUser.username });
 				}
 				if (!user.image) {
